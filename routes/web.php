fb6ccaa9c6911a836102d95e3632f30c15901fcf
Chetuman <tic-310114@utnay.edu.mx>
2025-02-08 15:36:59 -0600

    Creacion de plantilla
    Creacion del doc "plantilla" , sea grego la ruta plantilla a las rutas del proyecto, nada mas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('plantilla');
 });
+Route::get('/plantilla', function(){
+    return view('plantilla');
+})->name('plantilla');
