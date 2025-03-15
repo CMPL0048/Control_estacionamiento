023b76e50b9e515a0c77d36492123b2881842de4
Chetuman <tic-310114@utnay.edu.mx>
2025-03-15 12:41:10 -0600

    V2

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('plantilla');
+    return view('incio');
 });
 Route::get('/plantilla', function(){
     return view('plantilla');
